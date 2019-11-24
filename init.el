fdd8dbabdcae7189e84fc24f3d27b1155ac578eb
Zach Smith <z@zxmth.us>
2019-11-23 19:54:58 -0800

    Add dired configurations.

--- init.el
+++ init.el
@@ -1,4 +1,18 @@
-(package-initialize)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "https://marmalade-repo.org/packages/") t)
+
+(add-to-list 'package-archives
+             '("gnu" . "http://elpa.gnu.org/packages/") t)
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+(unless package--initialized (package-initialize))
+
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
 (cond
